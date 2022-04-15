144744484c874883c1e683e2ed0b1185f66a319f
Kiyini Joseph Balamazze <73903994+josephkb87@users.noreply.github.com>
2022-04-15 10:50:23 +0300

    Added example on concatenation Matrices and Matrix Generators
    Examples include concatenation Matrices and Matrix Generators

--- Array_and_Linear_Operations.m
+++ Array_and_Linear_Operations.m
@@ -10,11 +10,22 @@ x + 2y + 3z = 1
 4x + 5y + 6z = 1
 7x + 8y = 1
 
+%Concatenating Matrices
+A =
+1 2 3
+4 5 6
+7 8 9
+
+B = [A 10*A; -A[1 0 0; 0 1 0; 9 91]]
+
 % The coefficient matrix A is
 A =
 1 2 3
 4 5 6
 7 8 9
+
+%Matrix generators:
+
 
 And the vector b =
 1
